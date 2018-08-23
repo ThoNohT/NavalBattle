031ad2bb7361e6d1f0d1f402785e634682bd9879
ThoNohT <e.c.p.bataille@gmail.com>
2018-08-23 21:31:32 +0200

    Refactor

--- src/Main.hs
+++ src/Main.hs
@@ -8,68 +8,111 @@ import System.Console.ANSI
 import System.Console.Haskeline
 
 
--- CellState Ship Bomb
-data CellState = CellState Bool Bool
+type ShipPresent = Bool
+type Bombed = Bool
 
--- Board BattleStarted Rows
-data Board = Board Bool [ [ CellState ] ]
+data CellState = CellState ShipPresent Bombed
+type Board = [ [ CellState ] ]
 
 
-emptyBoard = Board False $ replicate 10 $ replicate 10 $ CellState False False
+data Phase = Placing | Bombing deriving (Eq)
+data Player = Player1 | Player2
+data Game = Game Phase Player Board Board 
+
+newGame :: Game
+newGame = Game Placing Player1 emptyBoard emptyBoard
+emptyBoard :: Board
+emptyBoard = replicate 10 $ replicate 10 $ CellState False False
+
+getBoard :: Game -> (Board, Board)
+getBoard (Game _ Player1 b1 b2) = (b1, b2)
+getBoard (Game _ Player2 b1 b2) = (b2, b1)
+
+setBoard :: Game -> Board -> Game
+setBoard (Game Placing Player1 _ b) new = Game Placing Player1 new b
+setBoard (Game Bombing Player1 b _) new = Game Bombing Player1 b new
+setBoard (Game Placing Player2 b _) new = Game Placing Player2 b new
+setBoard (Game Bombing Player2 _ b) new = Game Bombing Player2 new b
+
 
 main :: IO ()
-main = runInputT defaultSettings $ loop emptyBoard
+main = runInputT defaultSettings $ loop newGame
   where
-    loop :: Board -> InputT IO ()
-    loop board@(Board battleStarted rows) = do
-      outputStr $ displayBoard battleStarted board
+    loop :: Game -> InputT IO ()
+    loop game@(Game phase _ _ _) = let (ownBoard, opponentBoard) = getBoard game in      
+      do
+      outputStrLn $ displayBoard False opponentBoard
+      outputStrLn $ replicate 10 '#'
+      outputStrLn $ displayBoard True ownBoard
       minput <- getInputLine "% "
       case minput of
         Nothing -> return ()
         Just "quit" -> return ()
         Just input
-          | isPrefixOf "place " input -> case readMaybe (drop 6 input) of
-              Just (x,y) -> case placeShip board x y of
-                Left error -> do outputStrLn error
-                                 outputStrLn "Please try again."
-                                 loop board
-                Right new  -> loop new
-              Nothing -> do outputStrLn "Invalid input (x,y)."
-                            loop board
-          | isPrefixOf "bomb " input -> case readMaybe (drop 5 input) of
-            Just (x,y) -> case bomb board x y of
-              Left error -> do outputStrLn error
-                               outputStrLn "Please try again."
-                               loop board
-              Right (hit, new) -> do
-                outputStrLn $ if hit then "Hit" else "Miss"
-                loop new
-            Nothing -> do outputStrLn "Invalid input (x,y)."
-                          loop board
-          | isPrefixOf "start" input -> case startBattle board of
-              Left error -> do outputStrLn error
-                               outputStrLn "Please try again."
-                               loop board
-              Right new -> do outputStr clearScreenCode
-                              outputStrLn "Start the battle."
-                              loop new 
-          | otherwise -> do outputStrLn $ "Input was: " ++ input
-                            loop board 
+          | isPrefixOf "place " input -> if phase /= Placing
+            then do
+              outputStrLn "The battle is already started."
+              loop game
+            else handlePlace input ownBoard >>= loop . setBoard game
+          | isPrefixOf "bomb " input -> if phase /= Bombing
+            then do
+              outputStrLn "The battle is not yet started."
+              loop game
+            else handleBomb input opponentBoard >>= loop . setBoard game
+          | isPrefixOf "start" input -> handleStart game >>= loop
+          | otherwise -> loop game 
+
+handlePlace :: String -> Board -> InputT IO Board
+handlePlace input board =
+   case readMaybe (drop 6 input) of
+    Just (x,y) -> case placeShip board x y of
+      Left error -> do outputStrLn error
+                       outputStrLn "Please try again."
+                       return board
+      Right new  -> return new
+    Nothing -> do outputStrLn "Invalid input (x,y)."
+                  return board
+
+handleBomb :: String -> Board -> InputT IO Board
+handleBomb input board  =
+  case readMaybe (drop 5 input) of
+    Just (x,y) -> case bomb board x y of
+      Left error       -> do outputStrLn error
+                             outputStrLn "Please try again."
+                             return board
+      Right (hit, new) -> do
+        outputStrLn $ if hit then "Hit" else "Miss"
+        return new
+    Nothing -> do outputStrLn "Invalid input (x,y)."
+                  return board
+
+handleStart :: Game -> InputT IO Game
+handleStart game = case startBattle game of
+  Left error -> do outputStrLn error
+                   outputStrLn "Please try again."
+                   return game
+  Right new -> do outputStr clearScreenCode
+                  outputStrLn "Start the battle."
+                  return new 
+
 
 placeShip :: Board -> Int -> Int -> Either String Board
-placeShip (Board battleStarted board) x y
+placeShip board x y
       | x < 0 || x > 9 || y < 0 || y > 9 = Left "Out of bounds."
-      | battleStarted = Left "Battle already started"
       | otherwise = 
         let (pre, row:post) = splitAt y board
             (rpre, cell:rpost) = splitAt x row
         in case cell of
           CellState True _ -> Left "Ship already present."
-          _                -> Right $ Board False $ pre ++ [rpre ++ [CellState True False] ++ rpost] ++ post
+          _                -> Right $ pre ++ [rpre ++ [CellState True False] ++ rpost] ++ post
 
+countShips :: Board -> Int
+countShips = foldl' f 0 . concat
+             where f count (CellState True _) = 1 + count
+                   f count _ = count
 
 displayBoard :: Bool -> Board -> String
-displayBoard hideShips (Board _ rows) = unlines $ map (map $ displayCellState hideShips) rows
+displayBoard hideShips rows = unlines $ map (map $ displayCellState hideShips) rows
 
 displayCellState :: Bool -> CellState -> Char
 displayCellState hideShips state = case state of
@@ -78,18 +121,16 @@ displayCellState hideShips state = case state of
   CellState True True   -> '*'
   CellState False True  -> 'X'
 
-startBattle :: Board -> Either String Board
-startBattle (Board battleStarted rows)
-  | battleStarted = Left "Battle already started."
-  | otherwise     = Right $ Board True rows
+startBattle :: Game -> Either String Game
+startBattle (Game Placing _ b1 b2) = Right $ Game Bombing Player1 b1 b2
+startBattle (Game Bombing _ b1 b2) = Left "Battle already started."
 
 bomb :: Board -> Int -> Int -> Either String (Bool, Board)
-bomb (Board battleStarted board) x y
+bomb board x y
   | x < 0 || x > 9 || y < 0 || y > 9 = Left "Out of bounds."
-  | not battleStarted = Left "Battle not yet started"
   | otherwise = 
     let (pre, row:post) = splitAt y board
         (rpre, cell:rpost) = splitAt x row
     in case cell of
       CellState _ True -> Left "Already bombed"
-      CellState hasShip _ -> Right $ (hasShip, Board True $ pre ++ [rpre ++ [CellState hasShip True] ++ rpost] ++ post)
+      CellState hasShip _ -> Right $ (hasShip, pre ++ [rpre ++ [CellState hasShip True] ++ rpost] ++ post)
